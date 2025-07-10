7674d6156bfcd0f09dffd40904768ed6e7da41d8
samuelBright09 <samuelbrightsk@gmail.com>
2025-07-10 15:21:04 +0000

    Add Episodes Service and methods.
    -Add audio player to track audios and manage audio state.
    - No perfect styles yet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/layout/header.component';
 import { FooterComponent } from './shared/components/layout/footer.component';
+import { AudioPlayerComponent } from './audio-player/audio-player.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent
-  ],
+  imports: [RouterOutlet,CommonModule, HeaderComponent, FooterComponent, AudioPlayerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'podcast-web-app';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,13 @@ import { ConfessionsComponent } from './pages/confessions/confessions.component'
 import { TeamManagementComponent } from './pages/admin/team/team-management/team-management.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TeamComponent } from './pages/team/team.component';
+import { EpisodesListComponent } from './episodes-list/episodes-list.component';
+import { EpisodeDetailsComponent } from './episode-details/episode-details.component';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'episodes', component: EpisodesListComponent },
+  { path: 'episodes/:id', component: EpisodeDetailsComponent },
   { path: 'team', loadComponent: () => import('./pages/team/team.component').then(m => m.TeamComponent) },
   { path: 'confessions', component: ConfessionsComponent },
   { path: 'admin/login', component: LoginComponent },
